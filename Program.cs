4b5c0473ed794448a381db53e97771de346b3f4c
Artem <artem.mindsadyrov@actum.digital.com>
2023-02-28 10:09:09 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+public class GameOfLife
+{
+    public static void Main()
+    {
+        int M = 10,
+            N = 10;
+
+        // Initializing the grid.
+        int[,] grid =
+        {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 1, 1, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+
+        // Displaying the grid or print board
+        Console.WriteLine("Original Generation");
+        for (int i = 0; i < M; i++) // height
+        {
+            for (int j = 0; j < N; j++) // weight
+            {
+                if (grid[i, j] == 0)
+                    Console.Write(".");
+                else
+                    Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+        nextGeneration(grid, M, N);
+    }
+
+    // Function to print next generation
+    static void nextGeneration(int[,] grid, int M, int N)
+    {
+        int[,] future = new int[M, N];
+
+        // Loop through every cell
+        for (int l = 1; l < M - 1; l++)
+        {
+            for (int m = 1; m < N - 1; m++)
+            {
+                // finding no Of Neighbours
+                // that are alive
+                int aliveNeighbours = 0;
+                for (int i = -1; i <= 1; i++)
+                    for (int j = -1; j <= 1; j++)
+                        aliveNeighbours += grid[l + i, m + j];
+
+                // aliveNeighbours += grid[l - 1, m - 1];
+                // aliveNeighbours += grid[l, m - 1];
+                // aliveNeighbours += grid[l + 1, m - 1];
+                // aliveNeighbours += grid[l - 1, m];
+                // aliveNeighbours += grid[l + 1, m];
+                // aliveNeighbours += grid[l - 1, m + 1];
+                // aliveNeighbours += grid[l, m + 1];
+                // aliveNeighbours += grid[l + 1, m + 1];
+
+                // The cell needs to be subtracted
+                // from its neighbours as it was
+                // counted before
+                aliveNeighbours -= grid[l, m];
+
+                // Implementing the Rules of Life
+
+                // Cell is lonely and dies
+                if ((grid[l, m] == 1) && (aliveNeighbours < 2))
+                    future[l, m] = 0;
+                // Cell dies due to over population
+                else if ((grid[l, m] == 1) && (aliveNeighbours > 3))
+                    future[l, m] = 0;
+                // A new cell is born
+                else if ((grid[l, m] == 0) && (aliveNeighbours == 3))
+                    future[l, m] = 1;
+                // Remains the same
+                else
+                    future[l, m] = grid[l, m];
+            }
+        }
+
+        Console.WriteLine("Next Generation");
+
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (future[i, j] == 0)
+                    Console.Write(".");
+                else
+                    Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
